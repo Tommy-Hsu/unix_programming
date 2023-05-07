b1936c917177d442ea5783349dea935f8c3cd631
Tommy-Hsu <hpc880214@gmail.com>
2023-05-08 02:35:57 +0800

    Delete lab03/lab03_pub directory

--- lab03/lab03_pub/offset.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-elf = ELF('./chals')
-print("main =", hex(elf.symbols['main']))
-print("{:<12s} {:<8s} {:<8s}".format("Func", "GOT Offset", "Symbol Offset"))
-for g in elf.got:
-   if "code_" in g:
-      #print("{:4d}".format(int(g.replace("code_",""))), end=", ")
-      #print("\"{:<8x}\"".format(elf.got[g]), end=", ")
-       print("\"{:<8x}\"".format(elf.symbols[g]), end=", ")
